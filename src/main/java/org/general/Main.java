4bd06205de49be11a3369e9528a350366a808d1f
Lucalini <Lverweye@calpoly.edu>
2025-05-15 15:00:58 -0700

    Lab7 files

---
+++ src/main/java/org/general/Main.java
@@ -0,0 +1,23 @@
+package org.general;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+    private DrawPanel panel;
+
+    public Main() {
+        panel = new DrawPanel();
+        Repository.getInstance().addObserver(panel);
+        add(panel);
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
